ecf9901f3030ba25b1ab87bb451d5c01ca67d822
INDOnimous <40731452+INDOnimous@users.noreply.github.com>
2018-07-13 21:07:11 +0700

    Update Convert.sh

--- Convert.sh
+++ Convert.sh
@@ -1,16 +1,21 @@
-#! /data/data/com.termux
+#!/data/data/com.termux
 
 
+sleep 2
 figlet INDOnimous
-sleep 4
+sleep 2
 apt-get update && apt-get upgrade
+sleep 2
 termux-setup-storage
 sleep 2
 pkg install -y ffmpeg
-sleep 3
+sleep 2
 pkg install wget
-sleep 4
+sleep 2
 ~/bin
+sleep 2
 mkdir ~/storage/shared/INDOnimousTutorial
+sleep 1
 wget https://pastebin.com/raw/EDksGR1v -O ~/bin/termux-file-editor
+sleep 1
 dos2unix ~/bin/termux-file-editor
